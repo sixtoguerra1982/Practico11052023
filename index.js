6bc1200412b8512729f8cbf352f0a08e9e032ed4
Víctor Araya <victor.d.araya@gmail.com>
2023-05-12 23:16:14 -0400

    Se crea el metodo(accion) de la clase Cliente queda pendiente arreglar los throws erros

--- index.js
+++ index.js
@@ -25,16 +25,32 @@ class Impuesto {
 // CLASE CLIENTES
 class Cliente {
     constructor(nombre, Impuesto) {
+        console.log(typeof nombre)
+        // if (typeof nombre !== 'string') {
+        //     throw new Error('El valor debe ser un string')
+        // }
+        // if (typeof Impuesto !== 'Impuesto') {
+        //     throw new Error('El valor debe ser un string')
+        // }
         this.nombre = nombre
-        this.Impuesto = impuesto
+        this.Impuesto = Impuesto
+    }
+
+    // (monto_bruto_anual - deducciones) * 21%
+    calcularImpuesto() {
+        let montoBrutoAnual = this.Impuesto.montobrutoanual
+        let deducciones = this.Impuesto.deducciones
+
+        return (montoBrutoAnual - deducciones) * (21 / 100)
     }
 }
 
+// INSTANCIAMOS IMPUESTOS
 let impuesto = new Impuesto(1000, 15000)
-//console.log(impuesto.montobrutoanual)
-//console.log(impuesto.deducciones)
-//console.log(impuesto.monto_bruto_anual) 
+console.log(impuesto.montobrutoanual)
+console.log(impuesto.deducciones)
 
-let cliente = new Cliente('Nayareth Nain', impuesto);
-console.log(cliente.Impuesto.montobrutoanual);
-console.log(cliente.nombre);
+// INSTANCIAMOS CLIENTES
+let cliente = new Cliente('NayarethNain', impuesto)
+console.log(cliente.nombre)
+console.log(cliente.calcularImpuesto())
